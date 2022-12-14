c4c335ea5dc86854a65f974e3febb7f7a3e26a13
Moreira-Junior <junior_moreira88@hotmail.com>
2022-12-13 22:40:13 -0300

    fixing println info

--- src/aplicacaoConsole/Consultar.java
+++ src/aplicacaoConsole/Consultar.java
@@ -16,11 +16,11 @@ public class Consultar {
 		try {
 			System.out.println("\n---listar  veterinario com nome Fa");
 			System.out.println(Fachada.veterinarioTrechoDeNome("Fa"));
-			System.out.println("\n---listar  veterinario com especialidade poo1");
+			System.out.println("\n---listar  veterinario com especialidade poo2");
 			System.out.println(Fachada.listarVeterinarioPorEspecialidade("POO2"));
-			System.out.println("\n---listar pets cujo dono mora no endereco rua 6");
+			System.out.println("\n---listar pets cujo dono mora no endereco rua 5");
 			System.out.println(Fachada.petsPorRua("Rua 5"));
-			System.out.println("\n---Procedimentos cujos pets tem o tutor de nome Ana Beatriz1");
+			System.out.println("\n---Procedimentos cujos pets tem o tutor de nome Ana Beatriz2");
 			System.out.println(Fachada.procedimentosPorPetPorDono("Ana Beatriz2"));
 			System.out.println("\n---contratos da modalidade completo"
 					+ " cujo pet tem um tutor que more na paraiba");
